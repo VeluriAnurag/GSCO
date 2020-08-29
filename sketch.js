e5968e55effeef3934f92e305e17d63a3b35a02b
VeluriAnurag <68477626+VeluriAnurag@users.noreply.github.com>
2020-08-29 14:43:58 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
+var car,wall;
+var speed,weight;
+
+
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-  var car,wall;
-  var speed,weight;
+  createCanvas(1600,400);
+  //createSprite(400, 200, 50, 50);
+  
   speed=random(55,90);
   weight=random(400,1500);
   car=createSprite(50,200,50,50);
